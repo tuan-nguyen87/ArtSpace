b112fbbeef2f368cdf7d0f39d8646e02bce1ce5e
jencar1 <carreraj46@yahoo.com>
2024-02-15 14:50:19 -0800

    Update App.js

--- artspace/src/App.js
+++ artspace/src/App.js
@@ -42,6 +42,7 @@ function App() {
           <Route path="/Commissions" element={<Commissions />} />
           <Route path="/MessagingPage" element={<MessagingPage socket={socket} triggerNotification={triggerNotification} />} />
           <Route path="/LandingPage" element={<LandingPage />} />
+          <Route path="/Tutorial" element={<TutorialPage />} />
         </Routes>
       </Router>
     </div>
