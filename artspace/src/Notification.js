998d51dd83f8c055f210d89602c589ef82b82e88
bobby3218 <101081697+bobby3218@users.noreply.github.com>
2024-02-15 11:47:51 -0800

    update

---
+++ artspace/src/Notification.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles/Notification.css";
+
+const Notification = ({ message, type }) => {
+    if (!message) return null;
+
+    return (
+        <div className={`notification ${type}`}>
+            {message}
+        </div>
+    );
+};
+
+export default Notification;
