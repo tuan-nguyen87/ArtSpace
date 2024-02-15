50cf90221d852f7c40d17fbe122cf78fc366f7bf
bobby3218 <101081697+bobby3218@users.noreply.github.com>
2024-02-15 11:46:58 -0800

    UPdate

--- artspace/src/App.js
+++ artspace/src/App.js
@@ -1,22 +1,46 @@
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-//import HomePage from "./HomePage";
+import HomePage from "./HomePage";
 import NavigationBar from "./NavigationBar";
 import LandingPage from "./LandingPage";
 import Commissions from "./Commissions";
 import MessagingPage from "./MessagingPage";
-import DailyChallenge from "./DailyChallenge";
+import Notification from "./Notification";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3000");
 
 function App() {
+  const [notification, setNotification] = useState({ message: '', type: '' });
+
+  const triggerNotification = (message, type = 'info') => {
+    setNotification({ message, type });
+
+    setTimeout(() => {
+      setNotification({ message: '', type: '' });
+    }, 3000); // Dismiss notification after 3 seconds
+  };
+
+    useEffect(() => {
+    socket.on('newMessage', (data) => {
+      triggerNotification(data.message, 'success');
+    });
+    return () => {
+      socket.off('newMessage');
+    };
+  }, []);
+
   return (
     <div className="App">
+      <Notification message={notification.message} type={notification.type} />
       <Router>
         <NavigationBar />
         <Routes>
-          <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/Commissions" element={<Commissions />} />
-          <Route path="/MessagingPage" element={<MessagingPage />} />
-          <Route path="/DailyChallenge" element={<DailyChallenge/>} />
+          <Route path="/MessagingPage" element={<MessagingPage socket={socket} triggerNotification={triggerNotification} />} />
+          <Route path="/LandingPage" element={<LandingPage />} />
         </Routes>
       </Router>
     </div>
