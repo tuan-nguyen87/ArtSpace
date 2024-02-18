e6ff7e5c21f65aac6cb2c8cfb8989e5447eae9d6
jencar1 <carreraj46@yahoo.com>
2024-02-17 19:29:15 -0800

    update Tutorial page

--- artspace/src/TutorialPage.js
+++ artspace/src/TutorialPage.js
@@ -4,35 +4,30 @@ import './styles/Tutorial.css';
 
 function Tutorial() {
   return (
-    <div className="tutorial-container">
-      {/*<header>
-        <div>
-          <img src="/public/images/ArtSpace-Logo-Explore.png" alt="Logo" />
+    <div className="tutorial-body">
+      <div className="tutorial-container">
+        <div className="section-header">
+          Explore | Tutorials
         </div>
-  </header>*/}
-
-      <div className="section-header">
-        Explore | Tutorials
+        <main>
+          <div className="quadrant">
+            <img src="/Tutorial art/navigation-site-map.png" alt="Navigation Site" />
+            <h3>Navigation Site</h3>
+          </div>
+          <div className="quadrant">
+            <img src="/Tutorial art/art-tutorials-image.png" alt="Art Tutorials" />
+            <h3>Art Tutorials</h3>
+          </div>
+          <div className="quadrant">
+            <img src="/Tutorial art/educational-content-image.png" alt="Educational Content" />
+            <h3>Educational Content</h3>
+          </div>
+          <div className="quadrant">
+            <img src="/Tutorial art/resourceful-links-image.png" alt="Resourceful Links" />
+            <h3>Resourceful Links</h3>
+          </div>
+        </main>
       </div>
-
-      <main>
-        <div className="quadrant">
-          <img src="/Tutorial art/navigation-site-map.png" alt="Navigation Site" />
-          <h3>Navigation Site</h3>
-        </div>
-        <div className="quadrant">
-          <img src="/Tutorial art/art-tutorials-image.png" alt="Art Tutorials" />
-          <h3>Art Tutorials</h3>
-        </div>
-        <div className="quadrant">
-          <img src="/Tutorial art/educational-content-image.png" alt="Educational Content" />
-          <h3>Educational Content</h3>
-        </div>
-        <div className="quadrant">
-          <img src="/Tutorial art/resourceful-links-image.png" alt="Resourceful Links" />
-          <h3>Resourceful Links</h3>
-        </div>
-      </main>
     </div>
   );
 }
