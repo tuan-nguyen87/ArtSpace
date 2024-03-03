de5b9761ee7e32906ff8e5a5b458f418e1a1f8e6
tuan-nguyen87 <tuan.nguyen07@student.csulb.edu>
2024-03-02 16:15:19 -0800

    implement authentication of users

---
+++ artspace/src/Firebase/Firebase.js
@@ -0,0 +1,18 @@
+import { getAuth } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCcdvsv5A7UYztefSH1i3azm_YTPYmLnCU",
+  authDomain: "user-signin-b205b.firebaseapp.com",
+  projectId: "user-signin-b205b",
+  storageBucket: "user-signin-b205b.appspot.com",
+  messagingSenderId: "896905746270",
+  appId: "1:896905746270:web:ba9006c07ca56c49dea1cf",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app, auth };
