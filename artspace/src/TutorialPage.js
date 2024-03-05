e381ee085dee3a0ae9ca0f51dfb0714c7be3517e
jencar1 <carreraj46@yahoo.com>
2024-03-05 15:03:35 -0800

    tutorial page

--- artspace/src/TutorialPage.js
+++ artspace/src/TutorialPage.js
@@ -14,19 +14,19 @@ function Tutorial() {
         <main>
           <div className="quadrant">
             <img src="/Tutorial art/navigation-site-map.png" alt="Navigation Site" />
-            <h3>Navigation Site</h3>
+            <a>Navigation Site</a>
           </div>
           <div className="quadrant">
             <img src="/Tutorial art/art-tutorials-image.png" alt="Art Tutorials" />
-            <h3>Art Tutorials</h3>
+            <a>Art Tutorials</a>
           </div>
           <div className="quadrant">
             <img src="/Tutorial art/educational-content-image.png" alt="Educational Content" />
-            <h3>Educational Content</h3>
+            <a>Educational Content</a>
           </div>
           <div className="quadrant">
             <img src="/Tutorial art/resourceful-links-image.png" alt="Resourceful Links" />
-            <h3>Resourceful Links</h3>
+            <a>Resourceful Links</a>
           </div>
         </main>
       </div>
