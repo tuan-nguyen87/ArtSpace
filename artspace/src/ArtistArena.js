bcf673b77424b5922cdfde16cc82138646dbe132
Yasmine Valencia <132300844+YamTheCrow@users.noreply.github.com>
2024-03-07 13:55:09 -0800

    ArtistArena.js

--- artspace/src/ArtistArena.js
+++ artspace/src/ArtistArena.js
@@ -21,7 +21,21 @@ const ArtistArena = () => {
                             <p>Tell a story in 4 pictures!</p>
                             <p>Date</p>
                             <p>Time</p>
-                            <button type="submit">Compete!</button>
+                            {/* <button type="submit">Compete!</button> */}
+                            <div class ="popbox">
+                                <a class="popbutton" href="#popup1">Compete!</a>
+                            </div>
+                            <div id="popup1" class="overlay">
+                                <div class="popup">
+                                    <h2>4 Pannel Comics</h2>
+                                    <a class="close" href="#">&times;</a>
+                                    <div class="content">
+                                        <p>A picture is worth a thousand words. What can you say in four?
+                                        Tell us your four pannel story! </p>
+                                        <button type="submit">Image Upload Placeholder</button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="card">
@@ -33,7 +47,23 @@ const ArtistArena = () => {
                             <p>What OC's can you make?</p>
                             <p>Date</p>
                             <p>Time</p>
-                            <button type="submit">Compete!</button>
+                            {/* <button type="submit">Compete!</button> */}
+                            <div class ="popbox">
+                                <a class="popbutton" href="#popup2">Compete!</a>
+                            </div>
+                            <div id="popup2" class="overlay">
+                                <div class="popup">
+                                    <h2>Character Design</h2>
+                                    <a class="close" href="#">&times;</a>
+                                    <div class="content">
+                                        <p>Dive into the depths of your creativity and bring 
+                                            forth a character that resonates with you. 
+                                            Consider their appearance, quirks, and backstory, 
+                                            allowing their personality to shine through. </p>
+                                        <button type="submit">Image Upload Placeholder</button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -50,7 +80,27 @@ const ArtistArena = () => {
                             <p>Showcase the magic of your creativity!</p>
                             <p>Date</p>
                             <p>Time</p>
-                            <button type="submit">Compete!</button>
+                            {/* <button type="submit">Compete!</button> */}
+                            <div class ="popbox">
+                                <a class="popbutton" href="#popup3">Compete!</a>
+                            </div>
+                            <div id="popup3" class="overlay">
+                                <div class="popup">
+                                    <h2>Wizard April</h2>
+                                    <a class="close" href="#">&times;</a>
+                                    <div class="content">
+                                        <p> Let Wizard April ignite your creativity, 
+                                            as you delve into the realms of fantasy, 
+                                            weaving spells with your brush or pen to bring 
+                                            captivating tales of sorcery and wonder to life 
+                                            on canvas or paper. Don your wizard's hat, 
+                                            wield your artistic wand, and embark on an 
+                                            enchanting adventure that will leave a lasting 
+                                            mark on both your artistry and imagination.</p>
+                                        <button type="submit">Image Upload Placeholder</button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="card">
@@ -62,7 +112,26 @@ const ArtistArena = () => {
                             <p>It's all about dynamics! Figures in motion</p>
                             <p>Date</p>
                             <p>Time</p>
-                            <button type="submit">Compete!</button>
+                            {/* <button type="submit">Compete!</button> */}
+                            <div class ="popbox">
+                                <a class="popbutton" href="#popup4">Compete!</a>
+                            </div>
+                            <div id="popup4" class="overlay">
+                                <div class="popup">
+                                    <h2>Figuary Month</h2>
+                                    <a class="close" href="#">&times;</a>
+                                    <div class="content">
+                                        <p> Embrace the challenge of capturing dynamic poses, 
+                                            expressing emotion, and refining your artistic skills 
+                                            alongside a supportive community of fellow creators. 
+                                            Let Figuary inspire you to push your boundaries, 
+                                            hone your craft, and unleash your creativity as you 
+                                            embark on this captivating journey of artistic growth and 
+                                            discovery.</p>
+                                        <button type="submit">Image Upload Placeholder</button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
