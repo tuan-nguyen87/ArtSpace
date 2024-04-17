c221391e0f01d8eae6afe7c048325d7335f49bc3
tuan-nguyen87 <tuan.nguyen07@student.csulb.edu>
2024-04-17 12:05:56 -0700

    update profile page

--- artspace/src/ProfilePage.js
+++ artspace/src/ProfilePage.js
@@ -5,47 +5,32 @@ import "./styles/ProfilePage.css";
 const ProfilePage = () => {
   return (
     <div className="profile-page">
-      <div className="profile-container">
-        <div className="profile-section">
-          <img
-            className="pic"
-            src="/Homepage art/messages.png"
-            alt="Profile 1"
-          />
-          <a href="/Messaging2" className="message-text">
-            Messages
-          </a>
-        </div>
-        <div className="profile-section">
-          <img
-            className="pic"
-            src="/Homepage art/portfolio.png "
-            alt="Profile 2"
-          />
-          <a href="/Portfolio" className="message-text">
-            Portfolio
-          </a>
-        </div>
-        <div className="profile-section2">
-          <img
-            className="pic2"
-            src="/Homepage art/commissions.png"
-            alt="Profile 3"
-          />
-          <a href="/Commissions" className="message-text">
-            Commissions
-          </a>
-        </div>
-        <div className="profile-section2">
-          <img
-            className="pic2"
-            src="/Homepage art/collaborations.png"
-            alt="Profile 4"
-          />
-          <a href="#" className="message-text">
-            Collaborations
-          </a>
-        </div>
+      <div className="profile-section">
+        <img className="pic" src="/Homepage art/messages.png" alt="Profile 1" />
+      </div>
+      <div className="profile-section">
+        <img
+          className="pic"
+          src="/Homepage art/portfolio.png "
+          alt="Profile 2"
+        />
+      </div>
+      <div className="profile-section">
+        <img
+          className="pic"
+          src="/Homepage art/commissions.png"
+          alt="Profile 3"
+        />
+      </div>
+      <div className="profile-section">
+        <img
+          className="pic"
+          src="/Homepage art/collaborations.png"
+          alt="Profile 4"
+        />
+      </div>
+      <div className="profile-section">
+        <img className="pic" src="/Homepage art/for_sale.png" alt="Profile 5" />
       </div>
     </div>
   );
