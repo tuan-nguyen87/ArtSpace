2ca02899f981179758be02d492560e4ce4f6bd10
tuan-nguyen87 <tuan.nguyen07@student.csulb.edu>
2024-04-17 18:08:06 -0700

    save state

--- artspace/src/ProfileForSale.js
+++ artspace/src/ProfileForSale.js
@@ -1,11 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { auth } from "./Firebase/Firebase.js";
-import { getStorage, ref, uploadBytes } from "firebase/storage";
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  listAll,
+  getDownloadURL,
+} from "firebase/storage";
 import "./styles/ProfileForSale.css";
 
 const ProfileForSale = () => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   const [selectedPicture, setSelectedPicture] = useState(null); // State for selected picture
+  const [uploadedImages, setUploadedImages] = useState([]);
+
+  useEffect(() => {
+    // Function to fetch uploaded images from Firebase Storage
+    const fetchUploadedImages = async () => {
+      try {
+        // Get the current user
+        const currentUser = auth.currentUser;
+        if (!currentUser) {
+          console.error("User not authenticated.");
+          return;
+        }
+
+        // Get a reference to the current user's folder
+        const storage = getStorage();
+        const userFolderRef = ref(storage, `${auth.currentUser.uid}/forsale`);
+
+        // Get a list of all images in the user's folder
+        const imagesList = await listAll(userFolderRef);
+
+        // Retrieve download URLs for each image
+        const urls = await Promise.all(
+          imagesList.items.map(async (item) => {
+            return getDownloadURL(item);
+          })
+        );
+
+        // Update state with the download URLs
+        setUploadedImages(urls);
+      } catch (error) {
+        console.error("Error fetching uploaded images:", error);
+      }
+    };
+
+    // Fetch uploaded images when component mounts
+    fetchUploadedImages();
+  }, []);
 
   const handleUploadClick = () => {
     setIsPopupOpen(true);
@@ -43,7 +86,11 @@ const ProfileForSale = () => {
   return (
     <div className="ProfileForSale_Container">
       <h2 className="sale_text">Upload your work</h2>
-      <div className="sale_container"></div>
+      <div className="sale_container">
+        {uploadedImages.map((url, index) => (
+          <img key={index} src={url} alt={`Uploaded Image ${index}`} />
+        ))}
+      </div>
       <button className="profile_btn" onClick={handleUploadClick}>
         Upload
       </button>
