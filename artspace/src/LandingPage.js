f0105b5202b969abb8c47973eb95f886f2f0dd69
Yasmine Valencia <132300844+YamTheCrow@users.noreply.github.com>
2024-04-26 14:59:59 -0700

    Rough homepage extension

--- artspace/src/LandingPage.js
+++ artspace/src/LandingPage.js
@@ -44,6 +44,41 @@ const LandingPage = () => {
           </button>
         </section>
       </div>
+      <div className="team-container">
+        <div className="team">
+          <h2> Meet the Team</h2>
+          <img
+            className="team-images"
+            src="https://firebasestorage.googleapis.com/v0/b/user-signin-b205b.appspot.com/o/aHiUmTvYyyYt5femAM7mK2VC2OF2%2Fprofile-picture.jpg?alt=media&token=f08517aa-a356-464c-a7f2-c698b4135028"
+            alt="Profile Generic"
+          />
+          <img
+            className="team-images"
+            src="https://firebasestorage.googleapis.com/v0/b/user-signin-b205b.appspot.com/o/h6ivhFw60BN3Eo6Ai8o0saDVjQ73%2Fprofile-picture.jpg?alt=media&token=9df82f90-9e1a-4e0c-91e0-51899bd0d155"
+            alt="Profile Generic"
+          />
+          <img
+            className="team-images"
+            src="https://firebasestorage.googleapis.com/v0/b/user-signin-b205b.appspot.com/o/uZmTRz7zsdQCVGz7KsFAcM8S9Tc2%2Fprofile-picture.jpg?alt=media&token=956188b7-ca18-4326-baaa-61b181e23c68"
+            alt="Profile Generic"
+          />
+          <img
+            className="team-images"
+            src="https://firebasestorage.googleapis.com/v0/b/user-signin-b205b.appspot.com/o/jFug2d9bhAZCZ4Dt7g6b8c1SkHK2%2Fprofile-picture.jpg?alt=media&token=15b29b89-8c04-455f-a34c-2b4f90cfd92c"
+            alt="Profile Generic"
+          />
+          <img
+            className="team-images"
+            src="https://firebasestorage.googleapis.com/v0/b/user-signin-b205b.appspot.com/o/C2ofxCQZ0NMFu4QB98sW37gjnlB2%2Fprofile-picture.jpg?alt=media&token=899cc651-b015-4d3b-bf07-5bd6140f4346"
+            alt="Profile Generic"
+          />
+          <img
+            className="team-images"
+            src="https://firebasestorage.googleapis.com/v0/b/user-signin-b205b.appspot.com/o/3desXwADbUfKbu25QrFyUKaUMfx1%2Fprofile-picture.jpg?alt=media&token=1d622c4d-ea22-479e-9a5d-5bc1b56e87e0"
+            alt="Profile Generic"
+          />
+        </div>
+      </div>
     </div>
   );
 };
